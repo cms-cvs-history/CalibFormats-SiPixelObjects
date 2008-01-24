63f1d933d14efff2668caa8e4bdfe9393effc742
cmsbuild <>
2008-01-24 15:03:46 +0000

    This commit was manufactured by cvs2git to create tag 'CalibFormats- SiPixelObjects-CMSSW_1_8_0_pre10'.
    Sprout from master 2007-12-15 22:53:13 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Use /bin/bash, not /usr/local/bin/bash'
    Cherrypick from master 2008-01-24 15:03:44 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Add missing #include <cassert>, this revision derived from that included':
        src/PixelROCStatus.cc
        src/PixelTBMChannel.cc
    Cherrypick from master 2007-12-20 17:50:43 UTC Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch> 'removed try/catch statements as advertised in Hypernews by Bill Tanenbaum (see also https://hypernews.cern.ch/HyperNews/CMS/get/swDevelopment/1524.html )':
        test/SiPixelCalibConfigurationObjectMaker.cc

--- test/SiPixelCalibConfigurationObjectMaker.cc
+++ test/SiPixelCalibConfigurationObjectMaker.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Freya Blekman
 //         Created:  Wed Sep 19 13:43:52 CEST 2007
-// $Id: SiPixelCalibConfigurationObjectMaker.cc,v 1.5 2007/12/06 14:44:19 fblekman Exp $
+// $Id: SiPixelCalibConfigurationObjectMaker.cc,v 1.6 2007/12/10 13:58:32 fblekman Exp $
 //
 //
 
@@ -100,22 +100,11 @@ void SiPixelCalibConfigurationObjectMaker::analyze(const edm::Event&, const edm:
    edm::Service<cond::service::PoolDBOutputService> poolDbService;
    
    if(poolDbService.isAvailable()){
-     try{
-       if(poolDbService->isNewTagRequest("SiPixelCalibConfigurationRcd") ){
-	 poolDbService->createNewIOV<SiPixelCalibConfiguration>(myCalib,poolDbService->endOfTime(), "SiPixelCalibConfigurationRcd");
-       }
-       else{
-	 poolDbService->appendSinceTime<SiPixelCalibConfiguration>(myCalib,poolDbService->currentTime(), "SiPixelCalibConfigurationRcd");
-       }
+     if(poolDbService->isNewTagRequest("SiPixelCalibConfigurationRcd") ){
+       poolDbService->createNewIOV<SiPixelCalibConfiguration>(myCalib,poolDbService->endOfTime(), "SiPixelCalibConfigurationRcd");
      }
-     catch(const cond::Exception &er){
-       edm::LogError("SiPixelCalibConfigurationObjectMaker") << er.what() << std::endl;
-     }
-     catch(const std::exception &er){
-       edm::LogError("SiPixelCalibConfigurationObjectMaker") << "standard error caught: " << er.what() << std::endl;
-     }
-     catch(...){
-       edm::LogError("SiPixelCalibConfigurationObjectMaker") << " caught unknown error, please check what you're doing!" << std::endl;
+     else{
+       poolDbService->appendSinceTime<SiPixelCalibConfiguration>(myCalib,poolDbService->currentTime(), "SiPixelCalibConfigurationRcd");
      }
    }
 }
